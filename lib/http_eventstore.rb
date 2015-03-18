ee256ea8c3f402993adab6fd0998fe4feb94b200
rybex <tomek.rybka@gmail.com>
2015-03-18 01:16:29 +0100

    Required all files in main module.

--- lib/http_eventstore.rb
+++ lib/http_eventstore.rb
@@ -1,6 +1,15 @@
-require 'http_eventstore/version'
 require 'http_eventstore/configuration'
-require 'http_eventstore/api_client'
+require 'http_eventstore/client/api_client'
+require 'http_eventstore/client/connection'
+require 'http_eventstore/event_store_connection'
+require 'http_eventstore/models/event'
+require 'http_eventstore/models/page_url'
+require 'http_eventstore/actions/append_event_to_stream'
+require 'http_eventstore/actions/delete_stream'
+require 'http_eventstore/actions/read_all_stream_events'
+require 'http_eventstore/actions/read_all_stream_events_backward'
+require 'http_eventstore/actions/read_all_stream_events_forward'
+require 'http_eventstore/models/errors'
 
 module HttpEventstore
   class << self
