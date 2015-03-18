15016e2bdaa8f2036469beaca5dcca17b411c1a8
rybex <tomek.rybka@gmail.com>
2015-03-18 01:17:12 +0100

    Added page_size property to configuration file and renamed host to endpoint.

--- lib/http_eventstore/configuration.rb
+++ lib/http_eventstore/configuration.rb
@@ -1,11 +1,17 @@
 module HttpEventstore
   class Configuration
-    attr_accessor :host
+    attr_accessor :endpoint
     attr_accessor :port
+    attr_accessor :page_size
 
     def initialize
-      @host = '127.0.0.1'
+      @endpoint = '127.0.0.1'
       @port = 2113
+      @page_size = 20
+    end
+
+    def get_store_url
+      "http://#{@endpoint}:#{@port.to_s}"
     end
   end
 end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -3,27 +3,31 @@ require 'spec_helper'
 module HttpEventstore
   describe Configuration do
 
-    let(:new_host) {'test.host.com'}
-    let(:new_port) {3000}
+    let(:new_endpoint)  { 'test.host.com' }
+    let(:new_port)      { 3000 }
+    let(:new_page_size) { 50 }
 
     before :each do
       HttpEventstore.configure do |config|
-        config.host = new_host
+        config.endpoint = new_endpoint
         config.port = new_port
+        config.page_size = new_page_size
       end
     end
 
     specify 'sets the appropriate configuration properties' do
       config = HttpEventstore.configuration
-      expect(config.host).to eq(new_host)
+      expect(config.endpoint).to eq(new_endpoint)
       expect(config.port).to eq(new_port)
+      expect(config.page_size).to eq(new_page_size)
     end
 
     specify 'reset configuration properties do default values' do
       HttpEventstore.reset
       config = HttpEventstore.configuration
-      expect(config.host).to eq('127.0.0.1')
+      expect(config.endpoint).to eq('127.0.0.1')
       expect(config.port).to eq(2113)
+      expect(config.page_size).to eq(20)
     end
   end
 end
