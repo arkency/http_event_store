e8fcd432be3491f26d37696b2068e00cc6172bc7
rybex <tomek.rybka@gmail.com>
2015-03-23 21:50:15 +0100

    Removed unused method

--- lib/http_eventstore/actions/read_all_stream_events.rb
+++ lib/http_eventstore/actions/read_all_stream_events.rb
@@ -7,7 +7,7 @@ module HttpEventstore
 
     def call(stream_name)
       raise IncorrectStreamData if stream_name.nil? || stream_name.empty?
-      entries = get_stream_entries(stream_name)
+      entries = get_all_stream_entries(stream_name)
       return_events(entries)
     rescue ClientError => e
       raise StreamAlreadyDeleted if e.code == 410
@@ -17,25 +17,16 @@ module HttpEventstore
     private
     attr_reader :client
 
-    def get_stream_entries(stream_name, start_point = nil, entries = [])
-      stream_data = get_stream_data(stream_name, start_point)
+    def get_all_stream_entries(stream_name, start_point = nil, entries = [])
+      stream_data = get_stream_batch(stream_name, start_point)
       entries = append_entries(entries, stream_data['entries'])
       start_point = get_next_start_point(stream_data['links'], stream_name)
       return entries if start_point.nil?
-      get_stream_entries(stream_name, start_point, entries)
-    end
-
-    def get_stream_data(stream_name, start_point)
-      get_stream_batch(stream_name, start_point)
+      get_all_stream_entries(stream_name, start_point, entries)
     end
 
     def return_events(entries)
-      entries.collect do |entry|
-        event_id = entry['eventId']
-        type = entry['eventType']
-        data = JSON.parse(entry['data'])
-        Event.new(type, data, event_id)
-      end
+      ParseEntries.new.call(entries)
     end
   end
 end
