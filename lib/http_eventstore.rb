8374c172934a962b29bdf0b788f3167ee3765249
rybex <tomek.rybka@gmail.com>
2015-03-26 13:27:35 +0100

    Removed global configuration file. Removed models directory.

--- lib/http_eventstore.rb
+++ lib/http_eventstore.rb
@@ -1,9 +1,10 @@
-require 'http_eventstore/configuration'
-require 'http_eventstore/client/api_client'
-require 'http_eventstore/client/connection'
-require 'http_eventstore/client/errors_handler'
-require 'http_eventstore/event_store_connection'
-require 'http_eventstore/models/event'
+require 'http_eventstore/api/client'
+require 'http_eventstore/api/connection'
+require 'http_eventstore/api/errors_handler'
+require 'http_eventstore/connection'
+require 'http_eventstore/event'
+require 'http_eventstore/errors'
+require 'http_eventstore/endpoint'
 require 'http_eventstore/helpers/parse_entries'
 require 'http_eventstore/actions/append_event_to_stream'
 require 'http_eventstore/actions/delete_stream'
@@ -12,22 +13,3 @@ require 'http_eventstore/actions/read_all_stream_events_backward'
 require 'http_eventstore/actions/read_all_stream_events_forward'
 require 'http_eventstore/actions/read_stream_events_backward'
 require 'http_eventstore/actions/read_stream_events_forward'
-require 'http_eventstore/models/errors'
-
-module HttpEventstore
-  class << self
-    attr_writer :configuration
-  end
-
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
-  def self.reset
-    @configuration = Configuration.new
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-end
---
+++ lib/http_eventstore/api/client.rb
@@ -0,0 +1,50 @@
+module HttpEventstore
+  module Api
+    class ApiClient
+
+      def initialize(endpoint, port, page_size)
+        @endpoint = Endpoint.new(endpoint, port)
+        @page_size = page_size
+      end
+
+      attr_reader :endpoint, :page_size
+
+      def append_to_stream(stream_name, event, expected_version = nil)
+        headers = {"ES-EventType" => event.type, "ES-EventId" => event.event_id, "ES-ExpectedVersion" => "#{expected_version}"}.reject { |key, val| val.empty? }
+        make_request(:post, "/streams/#{stream_name}", event.data, headers)
+      end
+
+      def delete_stream(stream_name, hard_delete)
+        headers = {"ES-HardDelete" => "#{hard_delete}"}
+        make_request(:delete, "/streams/#{stream_name}", {}, headers)
+      end
+
+      def read_stream_backward(stream_name, start, count)
+        make_request(:get, "/streams/#{stream_name}/#{start}/backward/#{count}")
+      end
+
+      def read_stream_forward(stream_name, start, count, long_pool = false)
+        headers = long_pool == true ? {"ES-LongPoll" => "#{HttpEventstore.configuration.long_pool_time}"} : {}
+        make_request(:get, "/streams/#{stream_name}/#{start}/forward/#{count}", {}, headers)
+      end
+
+      def read_stream_page(uri)
+        make_request(:get, uri)
+      end
+
+      private
+
+      def make_request(method, path, body={}, headers={})
+        connection.send(method, path) do |req|
+          req.headers = req.headers.merge(headers)
+          req.body = body.to_json
+          req.params['embed'] = 'body' if method == :get
+        end.body
+      end
+
+      def request
+        @connection ||= Api::Connection.new(endpoint).call
+      end
+    end
+  end
+end
