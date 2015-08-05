09c0371e58056ec5f4c5ae93ed09c0c6cc1c0653
Justin Litchfield <litch@me.com>
2015-08-05 12:50:11 -0400

    Include the SourceEventUri

--- lib/http_eventstore/helpers/parse_entries.rb
+++ lib/http_eventstore/helpers/parse_entries.rb
@@ -14,12 +14,13 @@ module HttpEventstore
         id = entry['eventNumber']
         event_id = entry['eventId']
         type = entry['eventType']
+        source_event_uri = entry['id']
         return nil unless entry['data']
         data = JSON.parse(entry['data'])
         stream_name = entry['streamId']
         position = entry['positionEventNumber']
         created_time = entry['updated'] ? Time.parse(entry['updated']) : nil
-        Event.new(type, data, event_id, id, position, stream_name, created_time)
+        Event.new(type, data, source_event_uri, event_id, id, position, stream_name, created_time)
       end
     end
   end
