c96d1b183c62dea7afeb9be397ef96aded233a7d
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-05 17:15:17 +0100

    formatting: readable `create_event(entity)` method

--- lib/http_event_store/helpers/parse_entries.rb
+++ lib/http_event_store/helpers/parse_entries.rb
@@ -13,15 +13,17 @@ module HttpEventStore
       private
 
       def create_event(entry)
-        id = entry['eventNumber']
-        event_id = entry['eventId']
-        type = entry['eventType']
-        source_event_uri = entry['id']
         return nil unless entry['data']
-        data = JSON.parse(entry['data'])
-        stream_name = entry['streamId']
-        position = entry['positionEventNumber']
-        created_time = entry['updated'] ? Time.parse(entry['updated']) : nil
+
+        id               = entry['eventNumber']
+        event_id         = entry['eventId']
+        type             = entry['eventType']
+        source_event_uri = entry['id']
+        data             = JSON.parse(entry['data'])
+        stream_name      = entry['streamId']
+        position         = entry['positionEventNumber']
+        created_time     = entry['updated'] ? Time.parse(entry['updated']) : nil
+
         Event.new(type, data, source_event_uri, event_id, id, position, stream_name, created_time)
       end
     end
