80c98577830f32826aa8583196d1f66c7efe6732
Morgan Hallgren <morgan.hallgren@servicewell.se>
2017-04-12 14:44:59 +0200

    return events that don´t include any data

--- lib/http_event_store/helpers/parse_entries.rb
+++ lib/http_event_store/helpers/parse_entries.rb
@@ -13,16 +13,17 @@ module HttpEventStore
       private
 
       def create_event(entry)
-        return nil unless entry['data']
+        return nil unless entry['eventType']
 
         id               = entry['eventNumber']
         event_id         = entry['eventId']
         type             = entry['eventType']
         source_event_uri = entry['id']
-        data             = JSON.parse(entry['data'])
+        data             = entry['data'] ? JSON.parse(entry['data']) : nil
         stream_name      = entry['streamId']
         position         = entry['positionEventNumber']
         created_time     = entry['updated'] ? Time.parse(entry['updated']) : nil
+
 
         Event.new(type, data, source_event_uri, event_id, id, position, stream_name, created_time)
       end
