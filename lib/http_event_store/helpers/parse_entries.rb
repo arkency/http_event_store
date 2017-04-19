c276a03bd876301b7f921c7a1bb5a96542475446
Morgan Hallgren <morgan.hallgren@servicewell.se>
2017-04-19 13:50:41 +0200

    dont json parse empty string (data/meta_data) and return nil if eventType not set

--- lib/http_event_store/helpers/parse_entries.rb
+++ lib/http_event_store/helpers/parse_entries.rb
@@ -13,17 +13,17 @@ module HttpEventStore
       private
 
       def create_event(entry)
-        return nil unless entry['data']
+        return nil unless entry['eventType']
 
         id               = entry['eventNumber']
         event_id         = entry['eventId']
         type             = entry['eventType']
         source_event_uri = entry['id']
-        data             = JSON.parse(entry['data'])
+        data             = !entry['data'].nil? && !entry['data'].empty? ? JSON.parse(entry['data']) : nil
         stream_name      = entry['streamId']
         position         = entry['positionEventNumber']
         created_time     = entry['updated'] ? Time.parse(entry['updated']) : nil
-        meta_data        = entry['meta_data'] ? JSON.parse(entry['meta_data']) : nil
+        meta_data        = !entry['meta_data'].nil? && !entry['meta_data'].empty? ? JSON.parse(entry['meta_data']) : nil
 
         Event.new(type, data, meta_data, source_event_uri, event_id, id, position, stream_name, created_time)
       end
