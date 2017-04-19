f01f3f629a8493d76e2d693c603b56e5a3959604
Morgan Hallgren <morgan.hallgren@servicewell.se>
2017-04-19 07:27:16 +0200

    append_to_stream can handle meta data

--- spec/parse_entries_spec.rb
+++ spec/parse_entries_spec.rb
@@ -11,6 +11,7 @@ module HttpEventStore
         expect(events.length).to eq 1
         expect(events[0].type).to eq "entryCreated"
         expect(events[0].data).to eq({"a" => "1"})
+        expect(events[0].meta_data).to eq({"a" => "1"})
         expect(events[0].event_id).to eq "fbf4a1a1-b4a3-4dfe-a01f-6668634e16e4"
         expect(events[0].id).to eq 47
         expect(events[0].position).to eq 51
@@ -48,6 +49,7 @@ module HttpEventStore
           "eventId" => "fbf4a1a1-b4a3-4dfe-a01f-6668634e16e4",
           "eventType" => "entryCreated",
           "data" => "{\n  \"a\": \"1\"\n}",
+          "meta_data" => "{\n  \"a\": \"1\"\n}",
           "eventNumber" => 47,
           "positionEventNumber" => 51,
           "streamId" => 'entries',
