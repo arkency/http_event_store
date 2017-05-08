79022ca142aa0c4ed9a247fed2f5f47158e48bb4
Morgan Hallgren <morgan.hallgren@servicewell.se>
2017-05-08 10:57:11 +0200

    return {} instead of nil on data / metadata

--- lib/http_event_store/helpers/parse_entries.rb
+++ lib/http_event_store/helpers/parse_entries.rb
@@ -19,11 +19,11 @@ module HttpEventStore
         event_id         = entry['eventId']
         type             = entry['eventType']
         source_event_uri = entry['id']
-        data             = !entry['data'].nil? && !entry['data'].empty? ? JSON.parse(entry['data']) : nil
+        data             = !entry['data'].nil? && !entry['data'].empty? ? JSON.parse(entry['data']) : {}
         stream_name      = entry['streamId']
         position         = entry['positionEventNumber']
         created_time     = entry['updated'] ? Time.parse(entry['updated']) : nil
-        metadata        = !entry['metadata'].nil? && !entry['metadata'].empty? ? JSON.parse(entry['metadata']) : nil
+        metadata         = !entry['metadata'].nil? && !entry['metadata'].empty? ? JSON.parse(entry['metadata']) : {}
 
         Event.new(type, data, metadata, source_event_uri, event_id, id, position, stream_name, created_time)
       end
